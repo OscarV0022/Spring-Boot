b246a7982f458a14e7dec41c75165c517278a67c
OscarV0022 <mundococ0012@gmail.com>
2026-04-08 20:54:57 -0600

    Renacimiento del proyecto

--- app_nueva/src/test/java/com/example/demo/DemoApplicationTests.java
+++ app_nueva/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.hola_mundo_devops;
+package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class HolaMundoDevopsApplicationTests {
+class DemoApplicationTests {
 
 	@Test
 	void contextLoads() {
